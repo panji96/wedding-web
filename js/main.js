4cef0b12e5e165c810da9db2871df3c80dcbc1c7
panji96 <38599964+panji96@users.noreply.github.com>
2023-05-26 20:04:45 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(window).on("load",function(){
+	// home section slideshow
+	let slideIndex = $(".slide.active").index();
+	const slideLen = $(".slide").length;
+
+	function slideShow(){
+		$(".slide").removeClass("active").eq(slideIndex).addClass("active");
+		if(slideIndex == slideLen-1){
+			slideIndex = 0;
+		}
+		else{
+			slideIndex++;
+		}
+		setTimeout(slideShow,5000);
+	}
+	slideShow();
+})
+
+$(document).ready(function(){
+	// people filter
+	peopleFilter($(".filter-btn.active").attr("data-target"))
+	$(".filter-btn").click(function(){
+		if($(this).hasClass("active")){
+			return;
+		}
+		else{
+			peopleFilter($(this).attr("data-target"));
+		}
+	})
+	function peopleFilter(target){
+		$(".filter-btn").removeClass("active");
+		$(".filter-btn[data-target='"+target+"']").addClass("active");
+		$(".people-item").hide();
+		$(".people-item[data-category='"+target+"']").fadeIn();
+	}
+})
