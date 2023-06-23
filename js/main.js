7236912ba5ba6999638720cb2de5b12f94b5eb7b
panji96 <38599964+panji96@users.noreply.github.com>
2023-06-23 19:08:34 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -33,4 +33,60 @@ $(document).ready(function(){
 		$(".people-item").hide();
 		$(".people-item[data-category='"+target+"']").fadeIn();
 	}
+
+	// Gallery Popup
+	const wHeight = $(window).height();
+	$(".gallery-popup img").css("max-height",wHeight + "px");
+
+	let itemIndex = 0;
+	const totalGalleryItems = $(".gallery-item").length;
+
+	$(".gallery-item").click(function(){
+		itemIndex = $(this).index();
+		$(".gallery-popup").addClass("open");
+		$(".gallery-popup .gp-img").hide();
+		gpSlideShow();
+	})
+
+	$(".gp-controls .next").click(function(){
+		if(itemIndex == totalGalleryItems-1){
+			itemIndex = 0;
+		}
+		else{
+			itemIndex++;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function(){
+			gpSlideShow();
+		})
+	})
+
+	$(".gp-controls .prev").click(function(){
+		if(itemIndex == 0){
+			itemIndex = totalGalleryItems-1;
+		}
+		else{
+			itemIndex--;
+		}
+		$(".gallery-popup .gp-img").fadeOut(function(){
+			gpSlideShow();
+		})
+	})
+
+	function gpSlideShow(){
+		const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+		$(".gallery-popup .gp-img").fadeIn().attr("src",imgSrc);
+		$(".gp-counter").text((itemIndex+1) + "/" + totalGalleryItems);
+	}
+
+	// Close Gallery Popup
+	$(".gp-close").click(function(){
+		$(".gallery-popup").removeClass("open");
+	})
+
+	// hide gallery popup when clicked outside
+	$(".gallery-popup").click(function(event){
+		if($(event.target).hasClass("open")){
+			$(".gallery-popup").removeClass("open");
+		}
+	})
 })
